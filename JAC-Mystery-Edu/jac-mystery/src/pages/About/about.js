e1684b4b79efcd43d97321ddbe4ab363730f5b3a
Nuhamin-Tesfaye <nuhamintesfaye11@gmail.com>
2023-01-27 20:32:42 -0500

    adding contact and about page

---
+++ JAC-Mystery-Edu/jac-mystery/src/pages/About/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import doughHolt from '../../assets/doughHolt'
+const AboutPage = () => {
+
+    return(
+        <div> 
+           <h2> this is About page</h2>
+        </div>
+
+    );
+}
+export default AboutPage;
---
+++ JAC-Mystery-Edu/jac-mystery/src/pages/Contact/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactPage = () => {
+    return(
+        <div>
+            <h2>Contact us !</h2>
+            <p> 2645 N High St,Columbus,OH 43202</p>
+            <p>Located in : Mediterranean food imports</p>
+        </div>
+
+    );
+}
+export default ContactPage;
